aa2b0d2cf5c87e7ce693f272a66875c32399e0ec
JamesMutahi <mutahijames0@gmail.com>
2019-06-09 15:38:16 +0300

    Add directive

---
+++ src/app/profile-directive.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ProfileDirectiveDirective } from './profile-directive.directive';
+
+describe('ProfileDirectiveDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ProfileDirectiveDirective();
+    expect(directive).toBeTruthy();
+  });
+});
